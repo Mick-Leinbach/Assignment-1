2d3b2b1863f436ff4532c117c7d524b8e8e95360
Mick-Leinbach <cogsturner@gmail.com>
2024-02-02 10:30:00 -0600

    Running Project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
-using namespace std;
+#include "Assignment_1.h"
 
 int main() {
-	cout << "Hello world!";
+	string contents;
+	
+	// Attempt to open A1input.txt
+	fstream inFile;
+	inFile.open("A1input.txt");
+	if (!inFile.is_open()) {
+		cout << "Input file invalid.";
+	}
+
+	// Print out each line of file
+	while (getline(inFile, contents)) {
+		cout << contents << endl;
+	}
+
+	// Close file
+	inFile.close();
 }
